6f236d93f1b7eecb0f816f0cdf942589055185d7
larse514 <andrew.larsen@concordusa.com>
2019-06-18 13:47:50 -0500

    add initial integration test

---
+++ integration/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+##define inputs
+STACK=$1
+##define variables
+MACOS="Mac"
+
+NEWURL=`aws cloudformation describe-stacks \
+            --stack-name $STACK \
+            --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'ServiceEndpoint')]][0][*].{OutputValue:OutputValue}" \
+            --output text`
+
+echo $NEWURL
+
+STATUS=$(curl -s -o /dev/null -w '%{http_code}' --data '' $NEWURL/create)
+
+echo "${STATUS}"
+if [ $STATUS != "200" ]; then
+    echo "Incorrect status code ${STATUS} from api"
+    exit 1
