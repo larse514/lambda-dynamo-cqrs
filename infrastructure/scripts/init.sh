c02e425d218f86908ea92cc7279adb1421a81202
larse514 <andrew.larsen@concordusa.com>
2019-06-18 19:17:23 -0500

    set aws_region

--- infrastructure/scripts/init.sh
+++ infrastructure/scripts/init.sh
@@ -10,7 +10,7 @@ TF_DIR="$PROJECT_DIR/tf"
 terraform init \
     -backend-config "key=dynamo/base.tfstate" \
     -backend-config "bucket=$S3_BUCKET_NAME" \
-    -backend-config "region=$REGION" \
+    -backend-config "region=$AWS_REGION" \
     -reconfigure \
     -input=false \
     "$TF_DIR"
