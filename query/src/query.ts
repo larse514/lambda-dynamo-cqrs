6b03e930d9565b1408ee9e15bf3a15beeacf4cc8
larse514 <andrew.larsen@concordusa.com>
2019-06-18 23:24:47 -0500

    add query step

--- query/src/query.ts
+++ query/src/query.ts
@@ -1,10 +1,30 @@
 'use strict'
+import { DynamoDB } from 'aws-sdk'
+import { QueryOutput } from 'aws-sdk/clients/dynamodb';
+
+const dynamoDb = new DynamoDB.DocumentClient()
+const table = process.env.DYNAMODB_TABLE || '';
 
 export const handler = async (event: any, context: any): Promise<any> => {
-  // create a response
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({})
-  };
-  return Promise.resolve(response);
+  try {
+    // create a response
+    const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+      TableName: table
+    };
+
+    const result: QueryOutput = await dynamoDb.query(params).promise();
+    const customers = result.Items;
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ customers })
+    };
+    return Promise.resolve(response)
+  }
+  catch (err) {
+    console.log(err);
+    return Promise.reject({
+      statusCode: 500,
+      body: JSON.stringify({})
+    })
+  }
 }
