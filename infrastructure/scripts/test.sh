0fcd7c47315c0eab66c0bc411aa547faf8261578
larse514 <andrew.larsen@concordusa.com>
2019-06-18 19:12:38 -0500

    readd sudo hack

--- infrastructure/scripts/test.sh
+++ infrastructure/scripts/test.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# sudo gem install bundler
-bundle install
-bundle exec rake spec
+sudo gem install bundler
+sudo bundle install
+sudo bundle exec rake spec
