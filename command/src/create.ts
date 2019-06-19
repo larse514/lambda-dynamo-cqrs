82a39334d48374d5bc7978cd70482fef92f4260b
larse514 <andrew.larsen@concordusa.com>
2019-06-18 22:47:23 -0500

    use callback instead of await since sdk doesnt return promise

--- command/src/create.ts
+++ command/src/create.ts
@@ -7,40 +7,45 @@ const dynamoDb = new DynamoDB.DocumentClient()
 const table = process.env.DYNAMODB_TABLE || '';
 
 export const handler = async (event: any, context: any): Promise<any> => {
-  try {
-    const request: CustomerRequest = JSON.parse(event.body);
-    const timestamp = new Date().getTime();
-    console.log(request);
-    // create a response
-    const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
-      TableName: table,
-      Item: {
-        id: uuid(),
-        text: request.name,
-        checked: false,
-        createdAt: timestamp,
-        updatedAt: timestamp
-      }
-    };
-    
-    console.log(params);
 
-    await dynamoDb.put(params);
-    console.log('dynamo success');
+  const request: CustomerRequest = JSON.parse(event.body);
+  const timestamp = new Date().getTime();
+  console.log(request);
+  // create a response
+  const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
+    TableName: table,
+    Item: {
+      id: uuid(),
+      text: request.name,
+      checked: false,
+      createdAt: timestamp,
+      updatedAt: timestamp
+    }
+  };
+
+  console.log(params);
+
+  dynamoDb.put(params, (err, result) => {
+
+    if (err) {
+      console.log(err);
+      return Promise.reject({
+        statusCode: 500,
+        body: JSON.stringify({})
+      })
+    };
+
+    console.log(result);
+
     const response = {
       statusCode: 200,
       body: JSON.stringify({})
     };
+    
     return Promise.resolve(response);
-  }
+  });
 
-  catch (error) {
-    console.log(error);
-    return Promise.reject({
-      statusCode: 500,
-      body: JSON.stringify({})
-    })
-  }
+
 }
 
 interface CustomerRequest {
