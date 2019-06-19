a1e06410de76cd8713e5128c43891c80e43d40c6
larse514 <andrew.larsen@concordusa.com>
2019-06-19 10:43:14 -0500

    i understand the awspec format

--- infrastructure/spec/command_table_spec.rb
+++ infrastructure/spec/command_table_spec.rb
@@ -2,6 +2,7 @@ describe dynamodb_table('command-dev') do
   it { should exist }
   it { should have_attribute_definition('CustomerId').attribute_type('S') }
   it { should have_key_schema('CustomerId').key_type('HASH') }
-  its('stream_specification') { should eq 'NEW_IMAGE' }
+  its('stream_specification.stream_enabled') { should eq true }
+  its('stream_specification.stream_view_type') { should eq 'NEW_IMAGE' }
 
 end
